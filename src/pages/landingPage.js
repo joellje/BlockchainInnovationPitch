e87923b63ce500c1e636f32fa0cdcf66a642231c
marctan2705 <marctan2705@gmail.com>
2022-03-04 21:36:46 +0800

    landing page

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import "./landingPage.css";
+import logo from "../assets/nftuc.png";
+import gift from "../assets/giftEmoji.png"
+import party from "../assets/party.png"
 export default function landingPage() {
   return (
-    <div>
-        hi
+    <div className="mainpage">
+        <div className="pseudobar">
+          <span className="logoholder">
+            <img src={logo} className="logo"/>
+          </span>
+            <div className="social">
+              Social
+            </div>
+        </div>
+        <div className="body">
+          <img className="gift" src={gift}/>
+        </div>
+        <div className="party">
+          <img className="partyimg" src={party}/>
+        </div>
     </div>
   );
 }
