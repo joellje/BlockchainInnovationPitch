7665457462e859aaf4d3fdf0574ffdc8c0d7859b
marctan2705 <marctan2705@gmail.com>
2022-03-04 21:56:22 +0800

    buttons

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,6 +3,7 @@ import "./landingPage.css";
 import logo from "../assets/nftuc.png";
 import gift from "../assets/giftEmoji.png"
 import party from "../assets/party.png"
+import {Button} from "react-bootstrap"
 export default function landingPage() {
   return (
     <div className="mainpage">
@@ -13,8 +14,14 @@ export default function landingPage() {
             <div className="social">
               Social
             </div>
+            <Button variant="primary">
+              Fuck
+            </Button>{' '}
         </div>
         <div className="body">
+          <Button variant="primary">
+              Fuck2
+            </Button>{' '}
           <img className="gift" src={gift}/>
         </div>
         <div className="party">
