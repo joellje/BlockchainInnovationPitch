a1cea955956f18ea56b3ddbfbb3c3349efde5cd8
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-03-04 22:20:12 +0800

    edits

--- src/pages/voucherPage.js
+++ src/pages/voucherPage.js
@@ -1,11 +1,23 @@
 import React from "react";
 import classes from "./voucherPage.module.css";
+import logo from "../assets/nftuc.png";
+import wallet from "../assets/walletlogo.png";
+import background from "../assets/background.png";
 import Voucher from "../components/voucher";
 
-export default function wallet() {
+export default function walletPage() {
   return (
     <div>
-      <Voucher/>
+      <div className={classes.root}>
+        {/* NFT logo*/}
+
+        <img src={logo} className={classes.logo} />
+
+        <Voucher />
+
+        {/*Footer */}
+        <img src={background} className={classes.footer}></img>
+      </div>
     </div>
   );
 }
