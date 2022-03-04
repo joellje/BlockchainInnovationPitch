ad6e2f91ed6cec50ce2b7eac47abae58b3066fa4
matthew-chua <matthew@matts-stonks-mac.local>
2022-03-04 22:02:49 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import React from 'react';
-import LandingPage from "../src/pages/landingPage"
-import Wallet from "../src/pages/wallet"
-import WalletLanding from"../src/pages/walletlanding"
+import "./App.css";
+import React from "react";
+import LandingPage from "../src/pages/landingPage";
+import Wallet from "../src/pages/walletlanding";
+import Voucher from "../src/pages/wallet";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Wallet/>
+    <Router>
+      <Routes>
+        <Route exact path="/landing" element={<LandingPage />} />
+        <Route exact path="/wallet" element={<Wallet />} />
+        <Route exact path="/vouchers" element={<Voucher />} />
+        <Route path="/*" element={<Navigate to="/landing" />} />
+      </Routes>
+    </Router>
   );
 }
 
