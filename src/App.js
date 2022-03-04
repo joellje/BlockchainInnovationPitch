98f0d9e0bf283fd57f5f8985c653406f5f819f2f
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-03-04 22:00:51 +0800

    edits

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import React from 'react';
 import LandingPage from "../src/pages/landingPage"
 import Wallet from "../src/pages/wallet"
+import WalletLanding from"../src/pages/walletlanding"
 
 function App() {
   return (
-    <LandingPage></LandingPage>
+    <Wallet/>
   );
 }
 
--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -5,7 +5,6 @@ import Voucher from "../components/voucher";
 export default function wallet() {
   return (
     <div>
-      <div className={classes.root}>hELLo</div>
       <Voucher/>
     </div>
   );
